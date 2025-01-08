b33d88a056bc9057e01ae4fb919c90fa8bfbbdc7
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-08 14:54:06 +0000

    FizzBuzz
    complete the FizzBuzz exercise

--- SDE-website-template/js/index.js
+++ SDE-website-template/js/index.js
@@ -11,3 +11,20 @@ function HeadsOrTails() {
 }
 
 console.log(HeadsOrTails());
+
+function fizzBuzz(n){
+    for (i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else if (i % 3 === 0 ) {
+            console.log('Fizz');
+        } else {
+            console.log(i);
+        }
+        // console.log(`number : ${i}`);
+    }
+}
+
+fizzBuzz(100);
